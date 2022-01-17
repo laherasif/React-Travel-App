49f9312c3fe6cf8bf0398c2e3851aa9b4e23a855
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-17 14:53:47 +0500

    matrial ui component using

---
+++ src/component/Header/Map.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { AppBar, Typography, InputLabel, InputBase, Toolbar } from '@material-ui/core'
+import { Search } from '@material-ui/icons'
+import { Autocomplete } from '@react-google-maps/api'
+function Map() {
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar>
+
+                </Toolbar>
+
+            </AppBar>
+
+        </>
+    )
+}
+
+export default Map
