aedde12c5327ad4ebf1b9d12b6f97adbdee4346f
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-17 20:14:59 +0500

    add google api

--- src/component/Map/Map.jsx
+++ src/component/Map/Map.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import makeStyles from './style'
+import { Typography, Grid , } from '@material-ui/core'
+import  GoogleMapReact  from 'google-map-react'
 function Map() {
+    const classes = makeStyles()
     return (
-        <div>
-            map
+        <div className={classes.mapcontainer}>
+            <GoogleMapReact
+             bootstrapURLKeys={{key : "AIzaSyAvLWZqK0x_Gxf0l0iSuEn9tmbIJYBnzgw"}}
+             defaultCenter={''}
+             defaultZoom={14}
+             margin={[50 , 50 , 50 , 50]}
+             options={''}
+             onChange={''}
+            >
+
+            </GoogleMapReact>
         </div>
     )
 }
