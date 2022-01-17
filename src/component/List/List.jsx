660d2df854805ea14ed0322592e543fb034ab5e7
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-17 16:05:51 +0500

    add ui component

--- src/component/List/List.jsx
+++ src/component/List/List.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { Grid , Typography  } from '@material-ui/core'
 function List() {
     return (
-        <div>
-            list 
+        <div className={classes.Container}>
+              <Typography variant="h5">
+              Restura
+              </Typography>
         </div>
     )
 }
