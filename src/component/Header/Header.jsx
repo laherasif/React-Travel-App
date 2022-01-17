331917e543db02a104850c40356db5a19a24638d
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-17 15:38:24 +0500

    style js update

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
-import { AppBar, Typography, InputLabel, InputBase, Toolbar } from '@material-ui/core'
+import { AppBar, Typography, InputLabel, InputBase, Toolbar, Box } from '@material-ui/core'
 import { Search } from '@material-ui/icons'
 import { Autocomplete } from '@react-google-maps/api'
+import makeStyles from './style'
 function Header() {
+
+    const classes = makeStyles()
+
     return (
         <>
             <AppBar position="static">
-                <Toolbar>
-                 <Typography variant="h4">Travel Advisor</Typography>
+                <Toolbar className={classes.toolbar}>
+                    <Typography variant="h4" className={classes.title}>Travel Advisor</Typography>
+                    <Box className={classes.box} >
+                        <Typography varient="h5">
+                            Expolore more
+                    </Typography>
+
+                        {/* <Autocomplete> */}
+                        <div className={classes.input_wrapper}>
+                            <div className="search_">
+                                <Search />
+                            </div>
+                            <InputBase placeholder="...search" />
+                        </div>
+
+
+                        {/* </Autocomplete> */}
+                    </Box>
+
                 </Toolbar>
 
             </AppBar>
---
+++ src/component/Header/style.js
@@ -0,0 +1,24 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+
+    toolbar:{
+        display : "flex",
+        justifyContent:'space-between',
+        alignItems:'center'
+    },
+    box:{
+        display :'flex',
+        alignItems:'center'
+    },
+    input_wrapper:{
+        display :'flex',
+        alignItems:'center'
+    },
+
+      title:{
+          fontSize:'18px',
+          fontWeight: 500
+          
+      }
+}))
